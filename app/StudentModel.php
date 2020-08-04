65aca9d7ee40b66e9d40ba773b7f390fc6743e95
kelompok5pkl <Susilawatielsa5@gmail.com>
2020-08-04 09:05:15 +0700

    membuat method dicontroller dan model di laravel

---
+++ app/StudentModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StudentModel extends Model
+{
+    protected $table = 'students';
+    protected $guarded = [];
+}
+
---
+++ resources/views/admin.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hallo student</title>
+</head>
+<body>
+<tr>
+	<td>my name Student</td><br>
+	<td>my class XII RPL 2</td><br>
+	<td>my school SMKS MAHAPUTRA CERDAS UTAMA</td><br>
+</tr>
+
+</body>
+</html>
